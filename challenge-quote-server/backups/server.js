a5f06b7ac5d3c86ba5b149857bc2792c2abdae48
Shima <shimaadnanbasmenj@gmail.com>
2023-01-09 10:15:01 +0000

    the 4th challenge

--- challenge-quote-server/backups/server.js
+++ challenge-quote-server/backups/server.js
@@ -6,17 +6,25 @@ const express = require("express");
 const app = express();
 
 //load the quotes JSON
-const Quotes = require("./quotes.json");
+ const Quotes = require("../quotes.json");
 
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function(request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("Welcome to Shima s quote server");
 });
 
 //START OF YOUR CODE...
+
+app.get('/quotes',(req,res)=>{
+  res.json(Quotes)
+})
+const randomquotes=pickFromArray(Quotes)
+app.get('/quotes/random',(req,res)=>{
+  res.json(randomquotes)
+})
 
 //...END OF YOUR CODE
 
@@ -27,8 +35,8 @@ app.get("/", function(request, response) {
 function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
-
+const port=process.env.PORT || 9090
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(port, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
